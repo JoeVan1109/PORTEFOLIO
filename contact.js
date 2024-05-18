6a7f5954736f9b99716e1f56dd90990444f4d8b2
charlie <charlie.van1109@gmail.com>
2024-05-18 17:00:43 +0200

    finale version

--- contact.js
+++ contact.js
@@ -1,2 +1,11 @@
-let contactDesktop = document.querySelector('.contact-desktop');
+let contactCard = document.querySelector('.contact-desktop');
 let buttonContact = document.getElementById('buttonContact');
+
+buttonContact.addEventListener('click', () => {
+    console.log('clic');
+    if (window.innerWidth > 991) {
+        contactCard.classList.toggle('visible');
+    } else {
+        window.open('./contact-page.html');
+    }
+});
